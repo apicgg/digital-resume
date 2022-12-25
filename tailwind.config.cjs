36e65e9b6cb13b4fdd75fd167bb95ac1a5efb76b
Anurag Pramanik <p.anurag@outlook.com>
2022-12-25 16:18:32 +0530

    feat(work ): update work details

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,25 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
-  darkMode: 'class',
+  content: ["./*.html"],
+  darkMode: "class",
   theme: {
     screens: {
-      md: '600px',
+      md: "600px",
     },
     extend: {
       colors: {
-        mainTextColorDark: '#fff',
-        secondaryTextColorDark: '#adb0b1',
-        mainLinkColorDark: 'rgb(30, 190, 214)',
-        mainBorderColorDark: '#2b3031',
-        mainBgColorDark: '#131415',
-        mainTextColorLight: '#000',
-        secondaryTextColorLight: 'rgb(51 51 51)',
-        mainLinkColorLight: '#0da2b8',
-        mainBorderColorLight: 'rgb(218, 218, 218)',
-        mainBgColorLight: 'rgb(249, 250, 251)',
+        mainTextColorDark: "#fff",
+        secondaryTextColorDark: "#adb0b1",
+        mainLinkColorDark: "rgb(30, 190, 214)",
+        mainBorderColorDark: "#2b3031",
+        mainBgColorDark: "#131415",
+        mainTextColorLight: "#000",
+        secondaryTextColorLight: "rgb(51 51 51)",
+        mainLinkColorLight: "#0da2b8",
+        mainBorderColorLight: "rgb(218, 218, 218)",
+        mainBgColorLight: "rgb(249, 250, 251)",
       },
     },
   },
   plugins: [],
-}
+};
